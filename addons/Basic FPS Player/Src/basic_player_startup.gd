4ca416e2cf2cc49caa314d9d7faef20db214b893
Kip <acedia@kip.gay>
2025-02-13 11:43:56 +0800

    Added post processing

--- addons/Basic FPS Player/Src/basic_player_startup.gd
+++ addons/Basic FPS Player/Src/basic_player_startup.gd
@@ -95,6 +95,12 @@ func _physics_process(delta):
 		if velocity && is_on_floor():
 			head_bob_motion()
 		reset_head_bob(delta)
+		
+	if Input.is_action_just_pressed("ui_cancel"):
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _process(delta):
 	if Engine.is_editor_hint():
