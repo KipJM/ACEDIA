3be949f8133e1653f48f5e4e69140e213e399225
Kip <acedia@kip.gay>
2025-04-07 23:30:28 +0800

    Changing camera algorithm. TODO: FIX!!!!

--- Universal/Mirror/PlanarReflection.gd
+++ Universal/Mirror/PlanarReflection.gd
@@ -4,7 +4,7 @@ class_name PlanarReflector
 var reflect_camera : Camera3D
 var reflect_viewport: SubViewport
 @export var main_cam : Camera3D = null
-@export var reflection_camera_resolution: Vector2i = Vector2i(1920, 1080)
+@export var resolution_scale : float = 1
 @export var debug_ui: TextureRect
 
 
@@ -18,14 +18,14 @@ func _ready(): # TODO REMOVE
 func init_mirror():
 	reflect_viewport = SubViewport.new();
 	add_child(reflect_viewport);
-	reflect_viewport.size = reflection_camera_resolution;
+	update_viewport()
 	reflect_camera = Camera3D.new();
 	reflect_viewport.add_child(reflect_camera);
 	reflect_camera.cull_mask = 1;
 	reflect_camera.fov = main_cam.fov
 	reflect_camera.environment = main_cam.environment
 	reflect_camera.doppler_tracking = main_cam.doppler_tracking
-	reflect_camera.projection = main_cam.projection
+	reflect_camera.projection = Camera3D.PROJECTION_FRUSTUM
 	
 	# Cam attributes
 	var main_cam_attr = main_cam.attributes
@@ -83,18 +83,56 @@ func _process(_delta):
 	
 	reflect_camera.global_transform.origin = mirrored_pos
 
-	reflect_camera.basis = Basis(
-		main_cam.global_basis.x.normalized().bounce(reflection_plane.normal).normalized(),
-		main_cam.global_basis.y.normalized().bounce(reflection_plane.normal).normalized(),
-		main_cam.global_basis.z.normalized().bounce(reflection_plane.normal).normalized()
-	)
+	#reflect_camera.basis = Basis(
+		#main_cam.global_basis.x.normalized().bounce(reflection_plane.normal).normalized(),
+		#main_cam.global_basis.y.normalized().bounce(reflection_plane.normal).normalized(),
+		#main_cam.global_basis.z.normalized().bounce(reflection_plane.normal).normalized()
+	#)
+
+	# https://github.com/Norodix/GodotMirror/blob/master/addons/Mirror/Mirror/Mirror.gd
+	
+	# Transform the mirror camera to the opposite side of the mirror plane
+	var MirrorNormal = global_transform.basis.z	
+	var MirrorTransform =  Mirror_transform(MirrorNormal, global_transform.origin)
+	reflect_camera.global_transform = MirrorTransform * main_cam.global_transform
+	
+	# Look perpendicular into the mirror plane for frostum camera
+	reflect_camera.global_transform = reflect_camera.global_transform.looking_at(
+			reflect_camera.global_transform.origin/2 + main_cam.global_transform.origin/2, \
+			global_transform.basis.y
+		)
+	var cam2mirror_offset = global_transform.origin - reflect_camera.global_transform.origin
+	var near = abs((cam2mirror_offset).dot(MirrorNormal)) # near plane distance
+	near += 0.05 # avoid rendering own surface
+
+	# transform offset to camera's local coordinate system (frostum offset uses local space)
+	var cam2mirror_camlocal = reflect_camera.global_transform.basis.inverse() * cam2mirror_offset
+	var frostum_offset =  Vector2(cam2mirror_camlocal.x, cam2mirror_camlocal.y)
+	reflect_camera.set_frustum(mesh.size.x * scale.x, frostum_offset, near, 10000)
 	
 	DebugDraw3D.scoped_config().set_thickness(0.001);
 	# DEBUG
-	DebugDraw3D.draw_box(reflect_camera.global_position, reflect_camera.global_basis.get_rotation_quaternion(), Vector3.ONE * 0.2, Color.GREEN, true)
+	DebugDraw3D.draw_box(reflect_camera.global_position, reflect_camera.global_basis.get_rotation_quaternion(), Vector3.ONE * 0.2, Color.GREEN, true) 
+
+
+# n is the normal of the mirror plane
+# d is the offset from the plane of the mirrored object
+# Gets the transformation that mirrors through the plane with normal n and offset d
+func Mirror_transform(n : Vector3, d : Vector3) -> Transform3D:
+	var basisX : Vector3 = Vector3(1.0, 0, 0) - 2 * Vector3(n.x * n.x, n.x * n.y, n.x * n.z)
+	var basisY : Vector3 = Vector3(0, 1.0, 0) - 2 * Vector3(n.y * n.x, n.y * n.y, n.y * n.z)
+	var basisZ : Vector3 = Vector3(0, 0, 1.0) - 2 * Vector3(n.z * n.x, n.z * n.y, n.z * n.z)
+	
+	var offset = Vector3.ZERO
+	offset = 2 * n.dot(d)*n
+	
+	return Transform3D(Basis(basisX, basisY, basisZ), offset)	
+	pass
+
+
 
 func update_viewport() -> void:
-	reflect_viewport.size = get_viewport().size
+	reflect_viewport.size = get_viewport().size * resolution_scale
 
 
 func _on_screen_entered(area: Area3D) -> void:
