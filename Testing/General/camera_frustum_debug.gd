ee8916cac9b3904fe90f8d58d7c4a02335f5cc27
Kip <acedia@kip.gay>
2025-04-16 14:54:44 +0800

    V1.5 stage 1: 2D dynamic near plane dynamic near plane work when either axis is zero. new version will do 3d dynamic near plane

--- Testing/General/camera_frustum_debug.gd
+++ Testing/General/camera_frustum_debug.gd
@@ -2,9 +2,13 @@
 extends Camera3D
 
 func _ready() -> void:
-	DebugDraw3D.scoped_config().set_thickness(0.01);
+	DebugDraw3D.scoped_config().set_thickness(0.005);
 	
 func _process(delta: float) -> void:
+	DebugDraw3D.scoped_config().set_thickness(0.005);
 		# DEBUG
 	DebugDraw3D.draw_camera_frustum(self, Color.GREEN)
-	DebugDraw3D.draw_box(self.global_position, self.global_basis.get_rotation_quaternion(), Vector3.ONE * 0.2, Color.GREEN, true) 
+	DebugDraw3D.draw_box(self.global_position, self.global_basis.get_rotation_quaternion(), Vector3.ONE * 0.2, Color.GREEN, true)
+	DebugDraw3D.draw_arrow_ray(global_position, -(self.global_basis).z, 1, Color.PURPLE, 0.1) 
+	DebugDraw3D.draw_arrow_ray(global_position, -(self.global_basis).rotated(Vector3.UP, deg_to_rad(get_camera_projection().get_fov()/2)).z, 1, Color.ORANGE, 0.1) 
+	DebugDraw3D.draw_arrow_ray(global_position, -(self.global_basis).rotated(Vector3.RIGHT, -deg_to_rad(fov/2)).z, 1, Color.ORANGE, 0.1)
--- Testing/Mirror/mirror_v2_component.gd
+++ Testing/Mirror/mirror_v2_component.gd
@@ -24,12 +24,48 @@ func _process(delta: float) -> void:
 	reflect_cam.global_transform.origin = mirrored_pos;
 	
 	# rot
-	reflect_cam.global_basis = (Basis.FLIP_Y * Basis.FLIP_X * Basis.FLIP_Z * global_basis);
+	reflect_cam.global_transform.origin = mirrored_pos
+
+	reflect_cam.basis = Basis(
+		cam.global_basis.x.normalized().bounce(reflection_plane.normal).normalized(),
+		cam.global_basis.y.normalized().bounce(reflection_plane.normal).normalized(),
+		cam.global_basis.z.normalized().bounce(reflection_plane.normal).normalized()
+	)
 	
 	# near plane
 	var distance = -reflection_plane.distance_to(reflect_cam.global_position)
-	reflect_cam.near = distance;
 	
-	var offset = to_local(reflect_cam.global_position)
-	# X
-	reflect_cam.frustum_offset = offset
+	# Horizontal
+	var horiz_near: float;
+	var fov_h = deg_to_rad(reflect_cam.get_camera_projection().get_fov())
+	if reflect_cam.rotation.y > 0:
+		# plane-left frustum (camera ->)
+		var left_rot = abs(reflect_cam.rotation.y - (fov_h / 2))
+		var left_near = distance * (1/cos(left_rot)) * cos(fov_h/2)
+		
+		horiz_near = left_near
+	else:	
+		# plane-right frustum (camera <-)
+		var right_rot = abs(reflect_cam.rotation.y + (fov_h / 2))
+		var right_near = distance * (1/cos(right_rot)) * cos(fov_h/2)
+		
+		horiz_near = right_near
+	
+	# Vertical
+	var vert_near: float;
+	var fov_v = deg_to_rad(reflect_cam.fov)
+	
+	if reflect_cam.rotation.x < 0:
+		# plane-up frustum (camera v)
+		var up_rot = abs(reflect_cam.rotation.x + (fov_v / 2))
+		var up_near = distance * (1/cos(up_rot)) * cos(fov_v/2)
+		
+		vert_near = up_near;
+	else:
+		# plane-right frustum (camera v)
+		var down_rot = abs((fov_v / 2) - reflect_cam.rotation.x)
+		var down_near = distance * (1/cos(down_rot)) * cos(fov_v/2)
+		
+		vert_near = down_near
+	
+	reflect_cam.near = min(horiz_near, vert_near)
