067f8902f66c924bf806ffd645687b3356423753
Kip <acedia@kip.gay>
2025-06-25 11:08:22 +0800

    animated runners added for the finale animation still not sure if this is the final scene or a seperate finale scene

---
+++ Chase/chase_finale/animated_runners.gd
@@ -0,0 +1,27 @@
+@tool
+extends Node3D
+
+@export var anim_tree: AnimationTree
+@export var anim_key: StringName
+@export var lerp_speed: float
+
+var prev_position: Vector3
+var prev_velocity: Vector2
+
+func _ready() -> void:
+	prev_position = global_position
+
+func _process(delta: float) -> void:
+	var velocity = ((global_position - prev_position)	/ delta)
+	
+	var weight = 1 - exp(-lerp_speed * delta)
+	
+	var vel_orient: Vector3 = global_basis.inverse() * velocity # hopefully this will work?
+	
+	var current_vel = Vector2(vel_orient.x, -vel_orient.z)
+	
+	prev_velocity = lerp(prev_velocity, current_vel, weight)
+	
+	anim_tree.set(anim_key, prev_velocity)
+	
+	prev_position = global_position
