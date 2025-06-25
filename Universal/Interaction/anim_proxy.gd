c28babba4d150ce4737105271e61c11bb607cc75
Kip <acedia@kip.gay>
2025-06-25 22:15:39 +0800

    chase scene fixed

--- Universal/Interaction/anim_proxy.gd
+++ Universal/Interaction/anim_proxy.gd
@@ -1,6 +1,12 @@
 extends AnimationPlayer
 
 @export var anim_name: StringName
+@export var reset_on_ready: bool
+
+func _ready() -> void:
+	if (reset_on_ready):
+		play("RESET")
+		advance(0)
 
 func play_anim() -> void:
 	play(anim_name)
