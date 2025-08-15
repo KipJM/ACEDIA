a22a01bddbb74fcacbebf2275c3ca6cdc83e53f9
Kip <jeretech.jeremy@gmail.com>
2025-08-16 00:23:22 +0800

    Added reset handler for finale main menu. Proper ending is acknowledged now

--- MainMenu/new_game_plus_enabler.gd
+++ MainMenu/new_game_plus_enabler.gd
@@ -1,4 +1,25 @@
 extends Node
 
+@export var egg_button: Button
+@export var reset_button: Button
+
+@export_group("DEBUG")
+@export var debug_override: bool
+
 func _ready() -> void:
-	GraphicsSettings.new_game_plus = true
+	if debug_override:
+		print("AHAH")
+		GraphicsSettings.new_game_plus = true
+	
+	if not GraphicsSettings.new_game_plus:
+		print("egg intro")
+		# Only finished default route
+		GraphicsSettings.new_game_plus = true
+		egg_button.visible = true
+		reset_button.visible = false
+	else:
+		print("reset")
+		# Egg route finished
+		GraphicsSettings.new_game_plus = false
+		egg_button.visible = false
+		reset_button.visible = true
